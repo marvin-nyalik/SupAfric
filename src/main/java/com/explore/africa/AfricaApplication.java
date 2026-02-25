d7080db99889e321068c5862f3526909534812d4
MARVIN <o.silverygold@gmail.com>
2026-02-25 05:37:25 +0300

    Initialize project at spring.start

---
+++ src/main/java/com/explore/africa/AfricaApplication.java
@@ -0,0 +1,13 @@
+package com.explore.africa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explore/africa/AfricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.africa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
