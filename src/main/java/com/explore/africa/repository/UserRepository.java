471cec65ce41e9541b298b5f18b11030d7ce0957
MARVIN <o.silverygold@gmail.com>
2026-02-26 07:02:50 +0300

    User repository

---
+++ src/main/java/com/explore/africa/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.explore.africa.repository;
+
+import com.explore.africa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByIdAndName(Long aLong, String name);
+}
