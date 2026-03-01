904ca0088711af08ab52fe42f1470c7a33a12547
MARVIN <o.silverygold@gmail.com>
2026-03-01 20:42:24 +0300

    Test constraints in user entity

---
+++ src/test/java/com/explore/africa/model/UserValidationTest.java
@@ -0,0 +1,43 @@
+package com.explore.africa.model;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserValidationTest {
+    private Validator validator;
+
+    @BeforeEach
+    void setup() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+    }
+
+    @Test
+    void shouldFailIfNameTooShort(){
+        User user = new User("John");
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty());
+
+        assertThat(violations)
+                .extracting(ConstraintViolation::getMessage)
+                .containsExactly("Username must be at least 5 characters long");
+    }
+
+    @Test
+    void shouldPassWhenNameIsValid(){
+        User user = new User("Marvin");
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertTrue(violations.isEmpty());
+    }
+}
