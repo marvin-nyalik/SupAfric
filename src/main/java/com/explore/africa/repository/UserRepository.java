1b851d8bcfd910d308104e7abf23cb53c69b4770
MARVIN <o.silverygold@gmail.com>
2026-03-02 06:29:29 +0300

    Add Optional return

--- src/main/java/com/explore/africa/repository/UserRepository.java
+++ src/main/java/com/explore/africa/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByIdAndName(Long aLong, String name);
+    Optional<User> findByName(String name);
 }
