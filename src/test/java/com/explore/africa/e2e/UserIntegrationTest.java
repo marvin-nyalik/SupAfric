de7a49c8370893c9981a9fb0cc8496f166cab64b
MARVIN <o.silverygold@gmail.com>
2026-03-11 06:32:50 +0300

    E2E create and verify resource

--- src/test/java/com/explore/africa/e2e/UserIntegrationTest.java
+++ src/test/java/com/explore/africa/e2e/UserIntegrationTest.java
@@ -1,13 +1,17 @@
 package com.explore.africa.e2e;
 
+import com.explore.africa.dto.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +22,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserIntegrationTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void shouldCreateUser() throws Exception {
@@ -30,6 +37,31 @@ class UserIntegrationTest {
                             }
                             """))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Allan"));
+    }
+
+    @Test
+    void shouldCreateUserAndRetrieveIt() throws Exception {
+        MvcResult result = mockMvc.perform(post("/users/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                        """
+                            {
+                               "name": "Allan"
+                            }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Allan"))
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        UserResponse createdUser =
+                objectMapper.readValue(json, UserResponse.class);
+
+        Long userId = createdUser.id();
+        mockMvc.perform(get("/users/" + userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
                 .andExpect(jsonPath("$.name").value("Allan"));
     }
 }
